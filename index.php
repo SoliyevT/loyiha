2506ec14a514d7f037fae2f8ccd8d5cd7d8230db
solievt <solievtolqinjon59gmail.com>
2025-02-18 22:33:24 +0300

    rasmlar2

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ require 'includes/header.php';
 
 
 
+
+
+
 <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -26,7 +29,7 @@ require 'includes/header.php';
     </div>
 
     <div class="carousel-item">
-    <div class="foss dev2"></div>
+      <div class="foss dev2"></div>
       <div class="container">
         <div class="carousel-caption">
           <h1>Another example headline.</h1>
@@ -36,7 +39,7 @@ require 'includes/header.php';
       </div>
     </div>
     <div class="carousel-item">
-    <div class="foss dev3"></div>
+      <div class="foss dev3"></div>
       <div class="container">
         <div class="carousel-caption text-end">
           <h1>One more for good measure.</h1>
@@ -66,28 +69,25 @@ require 'includes/header.php';
   <!-- Three columns of text below the carousel -->
   <div class="row">
     <div class="col-lg-4">
-      <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-        <title>Placeholder</title>
-        <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-      </svg>
+      <div>
+        <img class="trc" src="/img/deere.jpg" alt="">
+      </div>
       <h2 class="fw-normal">Heading</h2>
       <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
       <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
     <div class="col-lg-4">
-      <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-        <title>Placeholder</title>
-        <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-      </svg>
+      <div>
+        <img class="trc" src="/img/holland.jpg" alt="">
+      </div>
       <h2 class="fw-normal">Heading</h2>
       <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
       <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
     <div class="col-lg-4">
-      <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-        <title>Placeholder</title>
-        <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-      </svg>
+    <div>
+        <img class="trc" src="/img/class.jpg" alt="">
+      </div>
       <h2 class="fw-normal">Heading</h2>
       <p>And lastly this, the third column of representative placeholder content.</p>
       <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
